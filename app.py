ddbfed24de87e396b8b2bbae7777ecaf05d3c34d
kavin <kavinkumarnkm007@gmail.com>
2021-02-22 22:23:02 +0530

    feat[social bot]: Initialize the bot and upload the photo

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ app.debug = True
 @app.route('/')
 def hello():
     print("asdf")
+    bot = Bot()
+    # bot.login()
+    # bot.upload_photo(caption="hello",photo="work.jpg")
+    bot.unfollow(user_id="leomessi")
+    print("completed")
     return 'Hello, World!'
 
 
 if __name__ == "__main__":
     app.run(debug = True)
+    # socketio.run(app)
 
